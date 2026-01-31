8f1a484f0d0355cb947ee82f30b132d98beff1d0
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-31 10:57:29 +0200

    add schemas

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,51 @@
-# Write your code here
+from datetime import date
+from decimal import Decimal
+from typing import Annotated, Literal
+from pydantic import BaseModel, Field
+from database.models import MovieStatusEnum
+
+
+class Actor(BaseModel):
+    id: int
+    name: Annotated[str, Field(max_length=255)]
+
+
+class Genre(BaseModel):
+    id: int
+    name: Annotated[str, Field(max_length=255)]
+
+
+class Language(BaseModel):
+    id: int
+    name: Annotated[str, Field(max_length=255)]
+
+
+class Country(BaseModel):
+    id: int
+    name: Annotated[str, Field(max_length=3)]
+    name: Annotated[str, Field(max_length=255)]
+
+
+class MovieBaseSchema(BaseModel):
+    name: Annotated[str, Field(max_length=255)]
+    date: date
+    score: float
+    overview: str
+
+
+class MovieListResponseSchema(MovieBaseSchema):
+    id: int
+
+
+class MovieListItemSchema(MovieBaseSchema):
+    status: Literal[MovieStatusEnum.RELEASED, MovieStatusEnum.POST_PRODUCTION, MovieStatusEnum.IN_PRODUCTION]
+    budget: Decimal = Field(max_digits=15, decimal_places=2)
+    revenue: float
+    country: Country
+    genres: list[Genre]
+    actors: list[Actor]
+    languages: list[Language]
+
+
+class MovieDetailSchema(MovieListItemSchema):
+    pass
